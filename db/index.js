f83ff31a54d7b33853625ffb564c04f4c089d99d
tijani sylla <tijanisylla@tijanis-Air.attlocal.net>
2021-06-16 16:12:39 -0500

    hello3

--- db/index.js
+++ db/index.js
@@ -1,3 +1,4 @@
+const { Client } = require('pg') 
 const client = new Client({
   connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
